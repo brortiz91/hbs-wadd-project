d98e02eed504e10c17bac7eed83fac8d47857fa8
Bryan Ortiz <bryanortiz91@gmail.com>
2017-06-15 13:07:23 -0400

    Portfolio Update
    added functionality to navigate to larger versions of photos on the portfolio page

---
+++ zoom.js
@@ -0,0 +1,4 @@
+//JavaScript for Zoom Images
+var figFilename = ["images/domrg.png", "images/dombar.png", "images/dommenu.png", "images/orderdeliv.png", "images/orderpickup.png", "images/contact.png", "images/excursion.png", "images/move.png", "images/fitness.png"];
+
+function pageSetup() {
