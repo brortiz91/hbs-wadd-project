ddf9a789d7946f125927e2698f72548b06f0ffcb
Bryan Ortiz <bryanortiz91@gmail.com>
2017-07-28 14:34:22 -0400

    Delete zoom.js
    not necessary for functionality of site at the moment

--- zoom.js
+++
@@ -1,4 +0,0 @@
-//JavaScript for Zoom Images
-var figFilename = ["images/domrg.png", "images/dombar.png", "images/dommenu.png", "images/orderdeliv.png", "images/orderpickup.png", "images/contact.png", "images/excursion.png", "images/move.png", "images/fitness.png"];
-
-function pageSetup() {
